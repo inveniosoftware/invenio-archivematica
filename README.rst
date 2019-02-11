52507726a45d287a3f4fea08793b9682f7f257e6
Ioannis Tsanaktsidis <ioannis.tsanaktsidis@cern.ch>
2019-02-11 11:28:00 +0100

    global: license change to MIT License
     * Closes #14.
    
    Signed-off-by: Ioannis Tsanaktsidis <ioannis.tsanaktsidis@cern.ch>

--- README.rst
+++ README.rst
@@ -1,25 +1,10 @@
 ..
     This file is part of Invenio.
-    Copyright (C) 2017 CERN.
+    Copyright (C) 2017-2019 CERN.
 
-    Invenio is free software; you can redistribute it
-    and/or modify it under the terms of the GNU General Public License as
-    published by the Free Software Foundation; either version 2 of the
-    License, or (at your option) any later version.
+    Invenio is free software; you can redistribute it and/or modify it
+    under the terms of the MIT License; see LICENSE file for more details.
 
-    Invenio is distributed in the hope that it will be
-    useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-    General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with Invenio; if not, write to the
-    Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
-    MA 02111-1307, USA.
-
-    In applying this license, CERN does not
-    waive the privileges and immunities granted to it by virtue of its status
-    as an Intergovernmental Organization or submit itself to any jurisdiction.
 
 =======================
  Invenio-Archivematica
@@ -31,14 +16,8 @@
 .. image:: https://img.shields.io/coveralls/inveniosoftware/invenio-archivematica.svg
         :target: https://coveralls.io/r/inveniosoftware/invenio-archivematica
 
-.. image:: https://img.shields.io/github/tag/inveniosoftware/invenio-archivematica.svg
-        :target: https://github.com/inveniosoftware/invenio-archivematica/releases
-
-.. image:: https://img.shields.io/pypi/dm/invenio-archivematica.svg
-        :target: https://pypi.python.org/pypi/invenio-archivematica
-
-.. image:: https://img.shields.io/github/license/inveniosoftware/invenio-archivematica.svg
-        :target: https://github.com/inveniosoftware/invenio-archivematica/blob/master/LICENSE
+.. image:: https://img.shields.io/pypi/v/invenio-archivematica.svg
+        :target: https://pypi.org/pypi/invenio-archivematica
 
 Invenio 3 module to connect Invenio to Archivematica
 
--- setup.py
+++ setup.py
@@ -1,26 +1,10 @@
 # -*- coding: utf-8 -*-
 #
 # This file is part of Invenio.
-# Copyright (C) 2017 CERN.
+# Copyright (C) 2017-2019 CERN.
 #
-# Invenio is free software; you can redistribute it
-# and/or modify it under the terms of the GNU General Public License as
-# published by the Free Software Foundation; either version 2 of the
-# License, or (at your option) any later version.
-#
-# Invenio is distributed in the hope that it will be
-# useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-# General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with Invenio; if not, write to the
-# Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
-# MA 02111-1307, USA.
-#
-# In applying this license, CERN does not
-# waive the privileges and immunities granted to it by virtue of its status
-# as an Intergovernmental Organization or submit itself to any jurisdiction.
+# Invenio is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
 
 """Invenio 3 module to connect Invenio to Archivematica"""
 
@@ -37,7 +21,6 @@ tests_require = [
     'invenio-accounts>=1.0.0',
     'isort>=4.3',
     'pydocstyle>=1.0.0',
-    'pytest-cache>=1.0',
     'pytest-cov>=1.8.0',
     'pytest-pep8>=1.0.6',
     'pytest>=2.8.3',
@@ -45,7 +28,9 @@ tests_require = [
 
 extras_require = {
     'docs': [
-        'Sphinx>=1.5.1,<1.6',  # TODO unpin see https://github.com/inveniosoftware/troubleshooting/issues/11
+        # TODO unpin see
+        # https://github.com/inveniosoftware/troubleshooting/issues/11
+        'Sphinx>=1.5.1,<1.6',
     ],
     'tests': tests_require,
 }
@@ -88,7 +73,7 @@ setup(
     description=__doc__,
     long_description=readme + '\n\n' + history,
     keywords='invenio TODO',
-    license='GPLv2',
+    license='MIT',
     author='CERN',
     author_email='info@inveniosoftware.org',
     url='https://github.com/inveniosoftware/invenio-archivematica',
@@ -141,7 +126,7 @@ setup(
     classifiers=[
         'Environment :: Web Environment',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+        'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
@@ -150,6 +135,6 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
-        'Development Status :: 1 - Planning',
+        'Development Status :: 5 - Production/Stable',
     ],
 )
